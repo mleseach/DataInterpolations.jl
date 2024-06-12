bda443f7bcff756aa345af4deb3109113f23a0b0
mleseach <113145496+mleseach@users.noreply.github.com>
2024-06-12 11:16:12 +0200

    add broadcast test

--- test/interface.jl
+++ test/interface.jl
@@ -18,3 +18,12 @@ A = LinearInterpolation(u, t)
 
 @variables t x(t)
 substitute(A(t), Dict(t => x))
+
+# Test broadcast
+u = 2.0collect(1:10)
+t = 1.0collect(1:10)
+A = LinearInterpolation(u, t)
+d = 1:2
+
+result = broadcast((t, d) -> DataInterpolations.derivative(A, t, d), t, d')
+@test DataInterpolations.derivative.(A, t, d') == result
